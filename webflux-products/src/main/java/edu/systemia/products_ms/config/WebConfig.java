ecf66e930f6892995139a290c229099ec1a9433b
josetanta <jose.tanta.27@unsch.edu.pe>
2024-12-19 19:18:53 -0500

    feat: add flux

--- webflux-products/src/main/java/edu/systemia/products_ms/config/WebConfig.java
+++ webflux-products/src/main/java/edu/systemia/products_ms/config/WebConfig.java
@@ -1,9 +1,26 @@
 package edu.systemia.products_ms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Order(1)
 @Configuration
 public class WebConfig {
+
+	@Bean
+	CorsWebFilter corsWebFilter() {
+		var config = new CorsConfiguration();
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		config.addAllowedOriginPattern("http://localhost:3000");
+		config.setAllowCredentials(true);
+
+		var source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsWebFilter(source);
+	}
 }
